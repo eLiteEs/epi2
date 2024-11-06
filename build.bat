d7f9bf5b14d1084664c2e06e0a256025c6bd2ce6
eLite <141034305+eLiteEs@users.noreply.github.com>
2024-11-06 20:00:34 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -8,4 +8,4 @@ echo "Analyzing license.cpp"
 g++ -Wall -Wextra -pedantic -lwinmm -lole32 -lgdi32 -lwinspool -lwlanapi -lWs2_32 -lwininet -lwinhttp -liphlpapi -lurlmon -lcomctl32 src/license.cpp -o temp_exec 2>&1
 echo "Compiling epi.cpp into epi2.exe"
 mkdir target
-g++ -lwinmm -lole32 -lgdi32 -lwinspool -lwlanapi -lWs2_32 -lwininet -lwinhttp -liphlpapi -lurlmon -lcomctl32 -static src/epi.cpp -o target/epi2
+g++ src/epi.cpp -o target/epi2 -static -lwinmm -lole32 -lgdi32 -lwinspool -lwlanapi -lWs2_32 -lwininet -lwinhttp -liphlpapi -lurlmon -lcomctl32
