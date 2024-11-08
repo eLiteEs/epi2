f5c88a79663c6ca0c5cafff52af1e107d83fa485
eLite <141034305+eLiteEs@users.noreply.github.com>
2024-11-08 21:37:51 +0100

    Maked "msgbox" function mare accesible

--- src/noerrorfile.cpp
+++ src/noerrorfile.cpp
@@ -1,7 +1,17 @@
-#include <windows.h>
 #include <iostream>
 #include <string>
 
-void showDialog(std::string message, std::string title) {
+#ifdef _WIN32
+    #include <windows.h> // For Windows MessageBox
+#else
+    #include <cstdlib>   // For Linux Zenity
+#endif
+
+void showDialog(const std::string& message, const std::string& title) {
+#ifdef _WIN32
     MessageBox(NULL, message.c_str(), title.c_str(), MB_OK | MB_ICONINFORMATION);
+#else
+    std::string command = "zenity --info --text=\"" + message + "\" --title=\"" + title + "\"";
+    std::system(command.c_str());
+#endif
 }
